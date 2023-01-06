eba8b0576effd77253d4141e32ba771748365ccf
Nweke Emmanuel <79476502+Manuel-co@users.noreply.github.com>
2023-01-06 14:17:08 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,35 +1,8 @@
- 
-function data() {
-    return {
-        task: '',
-        tasks: [ ],
-        
-        addTask() {
-            if (this.task) {
-                this.tasks.push({
-                    title: this.task,
-                    completed: false,
-                    id: Math.random()
-                })
-                this.task = ''
-            }
-        },
+function getTodosFromLocalStorage() {
+    const todos = localStorage.getItem("todos");
+    return todos ? JSON.parse(todos) : [];
+  }
 
-        complete(task) {
-            task.completed = !task.completed
-        },
-
-        get tasksSortedByCompleted() {
-            return this.tasks.sort((a, b) => a.completed - b.completed)
-        },
-
-        deleteTask(index) {
-            this.tasks = this.tasks.filter(
-                (task, taskIndex) => {
-                    return index !==  taskIndex
-                }
-            )
-        }
-    }
-
-}
+  function saveTodosToLocalStorage(todos) {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }
